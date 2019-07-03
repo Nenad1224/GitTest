598d09ddbe1e313a00f421c46efea2164bd21849
Obuka <Obuka@DESKTOP-F6T8J5C>
2019-07-03 18:52:43 +0200

    Brojac

--- src/java/beans/Brojac.java
+++ src/java/beans/Brojac.java
@@ -1,11 +1,19 @@
 
 package beans;
 
+import javax.enterprise.context.Dependent;
+import javax.inject.Named;
 
+@Named(value="brojac")
+@Dependent
 public class Brojac {
     
- public class Brojac{
+public Brojac(){
  
- }   
+ } 
+public String poruka(){
+
+return "bORUKA IZ BROJACA 1";
+}
     
 }
